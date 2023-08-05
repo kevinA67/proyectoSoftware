88dabeec3589223c2f81fe90257041c21e7427af
HeberGonzalez28 <ever.gonzalez83@gmail.com>
2023-08-04 23:05:11 -0600

    avance 4-8-23 PARTE 3

---
+++ src/vista/prueba2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vista;
+
+/**
+ *
+ * @author everg
+ */
+public class prueba2 {
+    
+    public void prueba2(){
+        System.out.println("Hola");
+        System.out.println("Hola2");
+        System.out.println("HOla3");
+    }
+    
+}
